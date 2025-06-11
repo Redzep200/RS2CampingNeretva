00ff6561c3b6733835c588f1f994fafe0c8f82ee
Redžep Trebović <redzept@gmail.com>
2025-06-11 14:51:29 +0200

    Subscription projekt

---
+++ CampingNeretva/CampingNeretva.Subscriber/Program.cs
@@ -0,0 +1,15 @@
+﻿using CampingNeretva.Model;
+using CampingNeretva.Model.Requests;
+using EasyNetQ;
+using EasyNetQ.DI;
+using System.Collections.Generic;
+
+var bus = RabbitHutch.CreateBus("host=localhost", x =>
+            x.Register<EasyNetQ.ISerializer>(_ => new EasyNetQ.Serialization.NewtonsoftJson.NewtonsoftJsonSerializer()));
+
+await bus.PubSub.SubscribeAsync<ReservationModel>("seminarski", msg => {
+    Console.WriteLine($"Reservation created: {msg.CheckInDate} - {msg.CheckOutDate}");
+});
+
+Console.WriteLine("Listening for messages, press <return> key to close!");
+Console.ReadLine();
