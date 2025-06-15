d0b9fd442cdad2934e556ba5363f580f2d699c6d
Redžep Trebović <redzept@gmail.com>
2025-06-15 20:29:19 +0200

    rabbitmq docker fix

--- CampingNeretva/CampingNeretva.Subscriber/Program.cs
+++ CampingNeretva/CampingNeretva.Subscriber/Program.cs
@@ -1,15 +1,23 @@
 ﻿using CampingNeretva.Model;
-using CampingNeretva.Model.Requests;
 using EasyNetQ;
 using EasyNetQ.DI;
-using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-var bus = RabbitHutch.CreateBus("host=rabbitmq;username=guest;password=guest;timeout=60", x =>
-    x.Register<ISerializer>(_ => new EasyNetQ.Serialization.NewtonsoftJson.NewtonsoftJsonSerializer()));
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var bus = RabbitHutch.CreateBus("host=rabbitmq;username=guest;password=guest;timeout=60", x =>
+            x.Register<ISerializer>(_ => new EasyNetQ.Serialization.NewtonsoftJson.NewtonsoftJsonSerializer()));
 
-await bus.PubSub.SubscribeAsync<ReservationModel>("seminarski", msg => {
-    Console.WriteLine($"Reservation created: {msg.CheckInDate} - {msg.CheckOutDate}");
-});
+        await bus.PubSub.SubscribeAsync<ReservationModel>("seminarski", msg =>
+        {
+            Console.WriteLine($"Reservation created: {msg.CheckInDate} - {msg.CheckOutDate}");
+        });
 
-Console.WriteLine("Listening for messages, press <return> key to close!");
-Console.ReadLine();
+        Console.WriteLine("Listening for messages. Press Ctrl+C to exit.");
+        await Task.Delay(Timeout.Infinite, CancellationToken.None); 
+    }
+}
